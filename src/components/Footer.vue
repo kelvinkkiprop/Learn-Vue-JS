d4b8f43bbcc63e08860a8e94f95c61e62a028d20
kelvinkkiprop <kelvinkiprop5@gmail.com>
2021-08-17 17:44:35 +0100

    BOOTSTRAP & BOOTSTRAP-VUE

---
+++ src/components/Footer.vue
@@ -0,0 +1,82 @@
+
+<!-------------------------------------------------template------------------------------------------------->
+<template>
+    <footer class="text-white">
+        <div class="bg-secondary p-2">
+            <b-container>
+                <b-row>
+                    <b-col>
+                        <h6>Team</h6>
+                        <ul style="list-style-type:square;">
+                            <li>{{team.member1}}</li>
+                            <li>{{team.member2}}</li>
+                        </ul>
+                    </b-col>
+                    <b-col>
+                        <h6>Team Locations</h6>
+                        <ol type="i">
+                            <li>{{team_location.location1}}</li>
+                            <li>{{team_location.location2}}</li>
+                        </ol>
+                    </b-col>
+                    <b-col>
+                        <h6>Social Media</h6>
+                        <p class="text-nowrap">
+                            <a v-bind:href="social_media.twitter" target="_blank"><b-icon-twitter></b-icon-twitter></a>&nbsp;
+                            <a v-bind:href="social_media.facebook" target="_blank"><b-icon-facebook></b-icon-facebook></a>&nbsp;
+                            <a v-bind:href="social_media.github" target="_blank"><b-icon-github></b-icon-github></a>&nbsp;
+                        </p>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
+        <p class="bg-dark text-center p-3">Learn Vue JS 2021</p>
+    </footer>
+</template>
+<!-------------------------------------------------./template------------------------------------------------->
+
+
+<!-------------------------------------------------script------------------------------------------------->
+<script>
+    export default {
+        data() {
+            return {
+                social_media:{
+                    twitter: 'https://twitter.com/kelvinkkiprop',
+                    github: 'https://github.com/kelvinkkiprop',
+                    facebook: 'https://www.facebook.com/profile.php?id=100002918520822',
+                },
+                team:{
+                    member1: 'Kelvin Kiprop',
+                    member2: 'PrudenceKelly Jepchumba',
+                },
+                team_location:{
+                    location1: 'Sfax, Tunisia',
+                    location2: 'Meru, Kenya',
+                },
+            };
+        },
+    };
+</script>
+<!-------------------------------------------------./script------------------------------------------------->
+
+
+<!-------------------------------------------------style------------------------------------------------->
+<style scoped lang="scss">
+
+    $white: #ffffff;
+    $black: #000000;
+
+    footer {
+        a{
+            color: $white;
+            border-radius: 45px;
+            padding: 0 2px 0 2px;
+        }
+        a:hover{
+            background-color: $white;
+            color: $black;
+        }
+    }
+</style>
+<!-------------------------------------------------./style------------------------------------------------->
---
+++ src/components/Navbar.vue
@@ -0,0 +1,65 @@
+
+<!-------------------------------------------------Template------------------------------------------------->
+<template>
+    <header>
+    <!-------------------------------------------------Navbar------------------------------------------------->
+    <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-container>
+            <b-navbar-brand href="#">Learn Vue JS</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+
+                    <!-- Left aligned nav items -->
+                <b-navbar-nav>
+                    <b-nav-item href="#">Home</b-nav-item>
+                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form>
+
+                    <b-nav-item-dropdown text="Components" right>
+                        <b-dropdown-item href="#">EN</b-dropdown-item>
+                        <b-dropdown-item href="#">ES</b-dropdown-item>
+                        <b-dropdown-item href="#">RU</b-dropdown-item>
+                        <b-dropdown-item href="#">FA</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                    <b-nav-item-dropdown right>
+                        <!-- Using 'button-content' slot -->
+                        <template #button-content>
+                            <em>Others</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+
+            </b-collapse>
+        </b-container>
+    </b-navbar>
+    <!-------------------------------------------------./Navbar------------------------------------------------->
+    </header>
+</template>
+<!-------------------------------------------------./Template------------------------------------------------->
+
+
+<!-------------------------------------------------Script------------------------------------------------->
+<script>
+    export default {
+        data() {
+            return {
+                twitter: 'https://twitter.com/kelvinkkiprop',
+                github: 'https://github.com/kelvinkkiprop',
+                facebook: 'https://www.facebook.com/profile.php?id=100002918520822',
+            };
+        },
+    };
+</script>
+<!-------------------------------------------------./Script------------------------------------------------->
